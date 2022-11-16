20f025fa3d98d0a6e07c82091f6fb24ef84e32d5
Jongco <narira0922@gmail.com>
2022-11-16 15:09:50 +0900

    비동기 테스트 코드 작성

--- unit-basic/src/test/async.test.js
+++ unit-basic/src/test/async.test.js
@@ -1,11 +1,40 @@
 const fetchProduct = require("../async");
 
-describe("async", () => {
-  it("reject", () => {
-    return fetchProduct("error").catch((error) => {
-      expect(error).toBe("network error");
+describe("비동기 테스트", () => {
+  describe("Promise", () => {
+    it("reject", () => {
+      return fetchProduct("error").catch((error) => {
+        expect(error).toBe("network error");
+      });
+    });
+
+    it("resolve", () => {
+      return fetchProduct().then((product) => {
+        expect(product).toEqual({ item: "Milk", price: 200 });
+      });
+    });
+
+    it("resolve2", (done) => {
+      fetchProduct().then((product) => {
+        expect(product).toEqual({ item: "Milk", price: 200 });
+        done();
+        //done을 사용하면 5초정도 느려짐
+      });
     });
   });
 
-  it("resolve", () => {});
+  describe("async-test", () => {
+    it("async", async () => {
+      const product = await fetchProduct();
+      expect(product).toEqual({ item: "Milk", price: 200 });
+    });
+
+    it("async-resolves", async () => {
+      return expect(fetchProduct()).resolves.toEqual({ item: "Milk", price: 200 });
+    });
+
+    it("async-reject", async () => {
+      return expect(fetchProduct("error")).rejects.toBe("network error");
+    });
+  });
 });

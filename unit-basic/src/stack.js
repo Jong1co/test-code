3bcfbf66da962d617500eec6e12249afecb1a1d6
Jongco <narira0922@gmail.com>
2022-11-17 12:22:15 +0900

    stack: TDD를 이용한 stack 구현

--- unit-basic/src/stack.js
+++ unit-basic/src/stack.js
@@ -1,15 +1,28 @@
 class Stack {
   constructor() {
     this.head = { value: undefined, next: undefined };
+    this.size = 0;
   }
 
   push(value) {
     if (this.head.value === undefined) {
       this.head.value = value;
+      this.size++;
       return;
     }
     const node = { value, next: this.head };
     this.head = node;
+    this.size++;
+  }
+
+  pop() {
+    const node = this.head;
+    if (this.size === 0) {
+      throw new Error("next value is not existed");
+    }
+    this.head = node.next;
+    this.size--;
+    return node.value;
   }
 }
 
--- unit-basic/src/test/stack.test.js
+++ unit-basic/src/test/stack.test.js
@@ -8,6 +8,34 @@ describe("stack", () => {
   });
 
   it("push", () => {
-    expect(stack.push("apple")).toEqual({ value: "apple", next: undefined });
+    stack.push("apple");
+
+    expect(stack.head).toEqual({ value: "apple", next: undefined });
+  });
+
+  it("push 2 datas", () => {
+    stack.push("apple");
+    stack.push("banana");
+
+    expect(stack.size).toBe(2);
+    expect(stack.head).toEqual({ value: "banana", next: { value: "apple", next: undefined } });
+  });
+
+  it("pop", () => {
+    expect(() => stack.pop()).toThrow();
+  });
+
+  it("push and pop", () => {
+    stack.push("apple");
+    stack.pop();
+
+    expect(stack.value).toBe(undefined);
+  });
+
+  it("push and pop", () => {
+    stack.push("apple");
+    stack.pop();
+
+    expect(stack.size).toBe(0);
   });
 });
